e79078b353b59811c5e8c486741dac80800ab0b8
maryammohebbi <maryam.mohebbi.jr@gmail.com>
2025-01-05 23:45:28 +0330

    project started. googleapis configs added. and some static ui also added

---
+++ src/app/page.jsx
@@ -0,0 +1,21 @@
+
+import { getSheetsData } from "@/services/sheetsService"
+
+
+export default async function Home() {
+
+  const data = await getSheetsData()
+
+  console.log("data:", data)
+
+
+  return (
+    <div className="flex flex-col items-center p-5">
+      <form className="flex gap-4 border-2 border-slate-500 rounded-2xl p-2 max-w-sm lg:max-w-xl shadow-xl">
+        <input className="p-4 w-[400px] border border-blue-200 rounded-xl" placeholder="Write your new todo..."/>
+        <button className="p-4 rounded-full bg-green-100">Add</button>
+      </form>
+      
+    </div>
+  )
+}
---
+++ src/services/sheetsService.js
@@ -0,0 +1,25 @@
+import { google } from "googleapis"
+
+export const getSheetsData = async ()=> {
+const auth = new google.auth.GoogleAuth({
+  credentials:{
+    client_email: process.env.GOOGLE_ACCOUNT_EMAIL,
+    private_key: process.env.GOOGLE_PRIVATE_KEY.replace(/\\n/g, "\n")
+  },
+  scopes: ["https://www.googleapis.com/auth/spreadsheets"]
+})
+const sheets = google.sheets({version: "v4", auth: await auth.getClient()})
+const range = "Sheet1!A:Z"
+
+try{
+  const response = await sheets.spreadsheets.values.get({
+    spreadsheetId: process.env.GOOGLE_SHEETS_ID,
+    range
+  })
+  return response.data.values
+} catch(error){
+  console.error("Error fetching sheets data:", error)
+  return []
+  
+ }
+}
